7dc1b21b5f1145df03c51052c8fa4c249a6ecbe6
KamilKorczek <korczek@gmail.com>
2022-08-27 23:16:48 +0200

    Add the computer movement and player movement variables

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
