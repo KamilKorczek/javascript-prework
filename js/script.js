e4d48858586313741690cdb92377d1be924cbcbf
KamilKorczek <korczek@gmail.com>
2022-08-28 08:40:05 +0200

    Modyfity file scrypt.js and add loop if>else

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+//Version with consol.log
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+
+//Version without consol.log
+printMessage('Mój ruch: ' + computerMove);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
+printMessage('Mój ruch: ' + computerMove);
